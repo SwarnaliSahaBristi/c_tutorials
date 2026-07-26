6b444d32cc34a59587231d31e30c64744f9c4bb1
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-26 17:53:24 +0600

    commit

--- Hello.c
+++ Hello.c
@@ -1,8 +1,12 @@
 # include<stdio.h> //-->preprocessor director
 
 int main(){
-    int age = 22;
-    float pi = 3.14;
-    char hashtag = 'hj';
+    // int age = 22;
+    // float pi = 3.14;
+    // char hashtag = 'hj'; //--> character e always single quato '' dite hobe
+    printf("Hello Swarnali \n"); //--> \n is for next line
+    printf("Hello Swarnali \n");
+    printf("Hello Swarnali \n");
+    printf("Hello Swarnali \n");
     return 0;
 }
