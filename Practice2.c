fc0713e0ca93d00b64662e593e0a89fc7a15483f
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-27 17:40:09 +0600

    commit

---
+++ Practice2.c
@@ -0,0 +1,11 @@
+// write a program to check if a number is divisible by 2 or not.
+ #include<stdio.h>
+ #include<math.h>
+
+ int main(){
+    int x;
+    printf("enter a number : ");
+    scanf("%d", &x);
+    printf("%d \n", x % 2 == 0);
+    return 0;
+ }
