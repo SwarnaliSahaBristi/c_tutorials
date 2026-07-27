9ce7596a04b6eaaa5a28cc02094e8749fbf60d51
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-27 17:56:47 +0600

    commit

--- Practice2.c
+++ Practice2.c
@@ -12,12 +12,30 @@
 
  //write a program to check if a number is odd or even.
 
-int main(){
+// int main(){
     //even -> 1
     //odd -> 0
-    int x;
-    printf("enter a number : ");
-    scanf("%d", &x);
-    printf("%d \n", x % 2 == 0);
-    return 0;
-}
+//     int x;
+//     printf("enter a number : ");
+//     scanf("%d", &x);
+//     printf("%d \n", x % 2 == 0);
+//     return 0;
+// }
+
+//if it's sunday & it's snowing -> true
+
+// int main(){
+//     int isSunday = 1;
+//     int isSnowing = 0;
+//     printf("%d \n", isSunday && isSnowing);
+//     return 0;
+// }
+
+//if it's monday or it's raining -> true
+ 
+// int main(){
+//     int isMonday = 1;
+//     int isRaining = 0;
+//     printf("%d \n", isMonday || isRaining);
+//     return 0;
+// }
