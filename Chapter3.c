d54b1ff3cc06fcb2e793284dfe7f3918c4f82f21
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-28 16:30:30 +0600

    commit

--- Chapter3.c
+++ Chapter3.c
@@ -10,32 +10,47 @@ marks <= 30 is fail
 
 #include<stdio.h>
 
+// int main(){
+//     int marks;
+//     printf("enter number(0-100) : ");
+//     scanf("%d", &marks);
+
+//     // if(marks > 30 && marks <= 100){
+//     //     printf("PASSED \n");
+//     // } else if(marks >=0 && marks <= 30) {
+//     //     printf("FAILED \n");
+//     // } else {
+//     //     printf("WRONG MARKS");
+//     // }
+
+//     // ternary
+//     // marks <= 30 ? printf("FAILED \n") : printf("PASSED \n");
+
+//     if(marks < 30) {
+//         printf("C \n"); 
+//     } else if (marks >= 30 && marks < 70) {
+//         printf("B \n");
+//     } else if (marks >= 70 && marks < 90) {
+//         printf("A \n");
+//     } else {
+//         printf("A+ \n");
+//     }
+    
+    
+//     return 0;
+// }
+
+//write a program to find if  character entered by user is upper case or not
+
 int main(){
-    int marks;
-    printf("enter number(0-100) : ");
-    scanf("%d", &marks);
+    char ch;
+    printf("Enter character: ");
+    scanf("%c", &ch);
 
-    // if(marks > 30 && marks <= 100){
-    //     printf("PASSED \n");
-    // } else if(marks >=0 && marks <= 30) {
-    //     printf("FAILED \n");
-    // } else {
-    //     printf("WRONG MARKS");
-    // }
-
-    // ternary
-    // marks <= 30 ? printf("FAILED \n") : printf("PASSED \n");
-
-    if(marks < 30) {
-        printf("C \n"); 
-    } else if (marks >= 30 && marks < 70) {
-        printf("B \n");
-    } else if (marks >= 70 && marks < 90) {
-        printf("A \n");
-    } else {
-        printf("A+ \n");
+    if(ch >= 'A' && ch <= 'Z'){
+        printf("UPPER CASE");
+    } else if( ch >= 'a' && ch <= 'z'){
+        printf("LOWER CASE");
     }
-    
-    
     return 0;
 }
