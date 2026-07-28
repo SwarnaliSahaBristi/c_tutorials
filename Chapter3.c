7ec64139bc0777acb968e9ccf56f2ca4ef442e8d
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-28 16:11:31 +0600

    commit

--- Chapter3.c
+++ Chapter3.c
@@ -1,3 +1,26 @@
 //conditional statements types => if-else & switch
 
 //ternary
+
+/*
+write a program to check if a student passed or failed 
+marks > 30 is pass
+marks <= 30 is fail
+*/
+
+#include<stdio.h>
+
+int main(){
+    int marks;
+    printf("enter number(0-100) : ");
+    scanf("%d", &marks);
+
+    if(marks > 30 && marks <= 100){
+        printf("PASSED \n");
+    } else if(marks >=0 && marks <= 30) {
+        printf("FAILED \n");
+    } else {
+        printf("WRONG MARKS");
+    }
+    return 0;
+}
