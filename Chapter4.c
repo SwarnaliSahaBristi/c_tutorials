1f69d4e3bc733bd2c4e1354e08feb782bcfc001d
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-29 09:04:09 +0600

    commit

--- Chapter4.c
+++ Chapter4.c
@@ -1,1 +1,17 @@
-// loop control instructions
+// loop control instructions -> for, while, do while
+
+#include<stdio.h>
+
+/*for loop
+for(initialisation; condition; updation){
+
+//do something
+
+}*/
+
+int main(){
+    for(int i=1; i<=5; i+1){
+        printf("Hello World \n");
+    }
+    return 0;
+}
