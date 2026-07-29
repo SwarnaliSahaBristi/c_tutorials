5176d064ac8541bf466abf82cf3fb465a8993b3f
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-29 09:10:59 +0600

    commit

--- Chapter4.c
+++ Chapter4.c
@@ -9,8 +9,10 @@ for(initialisation; condition; updation){
 
 }*/
 
+//print the numbers from 0 to 10
 int main(){
-    for(int i=10; i>=1; i=i-1){
+    //iterator ; counter
+    for(int i=0; i<=10; i=i+1){
         printf("%d \n", i);
     }
     return 0;
