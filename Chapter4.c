2d5f9bce6889d9beda5a371e026ee12946c6d5a5
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-29 10:52:46 +0600

    commit

--- Chapter4.c
+++ Chapter4.c
@@ -92,26 +92,40 @@ do{
 */
 
 //print the sum of first n Natural Numbers. also, print them in reverse.
-int main(){
-    // int i = 5;
-    // do
-    // {
-    //     printf("%d \n", i);
-    //     i--;
-    // } while (i>=0);
+// int main(){
+//     // int i = 5;
+//     // do
+//     // {
+//     //     printf("%d \n", i);
+//     //     i--;
+//     // } while (i>=0);
 
-    int n;
-    printf("enter number: ");
-    scanf("%d", &n);
+//     int n;
+//     printf("enter number: ");
+//     scanf("%d", &n);
 
-    // int sum= 0;
-    // for(int j=n; j>=1; j--){
-    //     sum= sum+j; //sum +=i
-    //     printf("%d \n", j);
-    // }
-    // printf("sum is %d \n", sum);
+//     // int sum= 0;
+//     // for(int j=n; j>=1; j--){
+//     //     sum= sum+j; //sum +=i
+//     //     printf("%d \n", j);
+//     // }
+//     // printf("sum is %d \n", sum);
 
-//print the table of a number input by the user    
+// //print the table of a number input by the user    
+//     for(int i=1; i<=10; i++){
+//         printf("%d \n", n*i);
+//     }
     
+//     return 0;
+// }
+
+//break statement---> exit the loop//
+int main(){
+    for(int i=1; i<=5; i++){
+        if(i==3){
+            break;
+        }
+        printf("%d \n", i);
+    }
     return 0;
 }
