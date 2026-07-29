7a1e2afb21e1abee7f8a9b53154b4da68f5e74b8
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-29 09:45:03 +0600

    commit

--- Chapter4.c
+++ Chapter4.c
@@ -71,11 +71,14 @@ int main(){
     printf("enter number: ");
     scanf("%d", &n);
 
-    int i=0;
-    while (i<=n)
-    {
-       printf("%d \n", i);
-       i++;
+    // int i=0;
+    // while (i<=n)
+    // {
+    //    printf("%d \n", i);
+    //    i++;
+    // }
+    for(int i=0; i<=n; i++){
+        printf("%d \n", i);
     }
     
     return 0;
