fa86b2f21976cba26ce4c9f23f696c385750fcd1
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-29 11:12:06 +0600

    commit

--- Chapter4.c
+++ Chapter4.c
@@ -170,11 +170,18 @@ int main(){
     // }
 
 //print all numbers from 1 to 10 except for 6.
-    for(int i=1; i<=10; i++){
-        if(i==6){   //skip
-            continue;
-        }
-        printf("%d \n", i);
-    }
+    // for(int i=1; i<=10; i++){
+    //     if(i==6){   //skip
+    //         continue;
+    //     }
+    //     printf("%d \n", i);
+    // }
+
+//print all the odd numbers from 5 to 50.
+    for(int i=5; i<=50; i++){
+        if(i % 2 !=0){   //skip
+            printf("%d \n", i);
+        } 
+    }   
     return 0;
 }
