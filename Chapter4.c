76c05b8a8aa6dd9a62b86b3e541251dc9754a94d
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-29 11:26:30 +0600

    commit

--- Chapter4.c
+++ Chapter4.c
@@ -185,15 +185,25 @@ int main(){
     // }  
     
 //print the factorial of a number n.
-    int n;
-    printf("enter number: ");
-    scanf("%d", &n);
+    // int n;
+    // printf("enter number: ");
+    // scanf("%d", &n);
     
-    int fact = 1;
-    for(int i=1; i<=n; i++){
-        fact = fact * i;
-    }
-    printf("final factorial is %d", fact);
+    // int fact = 1;
+    // for(int i=1; i<=n; i++){
+    //     fact = fact * i;
+    // }
+    // printf("final factorial is %d", fact);
+
+//print reverse of the table for a number n.    
+    // int n;
+    // printf("enter number: ");
+    // scanf("%d", &n);
+    // for(int i =10; i>=1; i--){
+    //     printf("%d \n", n*i);
+    // }
+
+//calculate the sum of all numbers between 5 and 50. including 5 & 50.    
 
     return 0;
 }
