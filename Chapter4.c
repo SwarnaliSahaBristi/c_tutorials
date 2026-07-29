c119c47ab780a236d49b863d450d7280420ff537
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-07-29 09:33:19 +0600

    commit

--- Chapter4.c
+++ Chapter4.c
@@ -10,42 +10,55 @@ for(initialisation; condition; updation){
 }*/
 
 //print the numbers from 0 to 10
-int main(){
-    //iterator ; counter
-    //i=i+1 shorthand i++
+// int main(){
+//     //iterator ; counter
+//     //i=i+1 shorthand i++
 
-    // for(int i=0; i<=10; i++){
-    //     printf("%d \n", i);
-    // }
+//     // for(int i=0; i<=10; i++){
+//     //     printf("%d \n", i);
+//     // }
 
-    //increment operator
-    //++i (pre-increment)
-    //i++ (post-increment)
+//     //increment operator
+//     //++i (pre-increment)
+//     //i++ (post-increment)
 
-    //--i (pre-decrement)
-    //i-- (post-decrement)
+//     //--i (pre-decrement)
+//     //i-- (post-decrement)
 
-    int i=1;
-    // printf("%d \n", i++); //use, then increase
-    // printf("%d \n", i);
+//     // int i=1;
+//     // printf("%d \n", i++); //use, then increase
+//     // printf("%d \n", i);
 
-    // printf("%d \n", i--);
-    // printf("%d \n", i);
+//     // printf("%d \n", i--);
+//     // printf("%d \n", i);
 
-    // printf("%d \n", ++i); //increase then use
-    // printf("%d \n", i);
+//     // printf("%d \n", ++i); //increase then use
+//     // printf("%d \n", i);
 
-    // printf("%d \n", --i);
-    // printf("%d \n", i);
+//     // printf("%d \n", --i);
+//     // printf("%d \n", i);
 
-    //loop counter can be float or character
-    // for(float i=1.0; i<=5; i++){
-    //     printf("%f \n", i);
-    // }
+//     //loop counter can be float or character
+//     // for(float i=1.0; i<=5; i++){
+//     //     printf("%f \n", i);
+//     // }
     
-    // for(char ch='a'; ch<='z';ch++){
-    //     printf("%c \n", ch);
-    // }
+//     // for(char ch='a'; ch<='z';ch++){
+//     //     printf("%c \n", ch);
+//     // }
+    
+//     return 0;
+// }
+
+//while loop
+
+/*
+while(condition){
+   do something
+}
+*/
+
+int main(){
     
     return 0;
 }
