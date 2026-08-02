6d8a966d1391b6412022b4ca7563b0b13bbf0885
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-08-02 10:20:30 +0600

    commit

---
+++ PracticeLeve1.c
@@ -0,0 +1,16 @@
+/*
+🟢 Level 1 - Variables & Input/Output
+
+2.Take two integers and print:
+Sum
+Difference
+Product
+Quotient (integer division)
+Remainder
+3.Take a float representing Celsius and convert it to Fahrenheit.
+4.Take a character as input and print its ASCII value.
+5.Swap two integers using a third variable.
+6.Swap two integers without using a third variable.
+*/
+
+//1.Take an integer as input and print its square.
